0510fc3bd062caf799eac835bf7706ea648c0995
Jason Roelofs <jasongroelofs@gmail.com>
2013-04-28 21:11:21 -0400

    Start working on storing mesh info on the graphics card
    I'm going with glDrawArrays for now as it's easier to work with than
    glDrawElements and I'll most likely need both anyway. This though
    requires a full set of verticies for the default cube as there's no
    vertex sharing with Array drawing.

--- src/core/mesh.go
+++ src/core/mesh.go
@@ -5,4 +5,9 @@ type Mesh struct {
 	VertexList []float32
 	ColorList  []float32
 	IndexList  []int32
+
+	// Internal renderer storage
+	VertexArrayObj uint32
+	VertexBuffer   uint32
+	ColorBuffer    uint32
 }
--- src/platform/opengl_renderer.go
+++ src/platform/opengl_renderer.go
@@ -2,6 +2,7 @@ package platform
 
 import (
 	"core"
+	"github.com/go-gl/gl"
 )
 
 type OpenGLRenderer struct {
@@ -9,5 +10,20 @@ type OpenGLRenderer struct {
 }
 
 func (self *OpenGLRenderer) LoadMesh(mesh *core.Mesh) {
+	vertexArrayObj := gl.GenVertexArray()
+	vertexArrayObj.Bind()
 
+	vertexBuffer := gl.GenBuffer()
+	vertexBuffer.Bind(gl.ARRAY_BUFFER)
+	defer vertexBuffer.Unbind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, len(mesh.VertexList) * 4, mesh.VertexList, gl.STATIC_DRAW)
+
+	colorBuffer := gl.GenBuffer()
+	colorBuffer.Bind(gl.ARRAY_BUFFER)
+	defer colorBuffer.Unbind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, len(mesh.ColorList) * 4, mesh.ColorList, gl.STATIC_DRAW)
+
+	//mesh.VertexArrayObj = vertexArrayObj.(gl.GLuint)
+	//mesh.VertexBuffer = vertexBuffer.(gl.GLuint)
+	//mesh.ColorBuffer = colorBuffer.(gl.GLuint)
 }
