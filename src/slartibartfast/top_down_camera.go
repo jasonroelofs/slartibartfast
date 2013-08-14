cbfbc20c96ada3079a3bb80727e30a50fe8d77da
Jason Roelofs <jasongroelofs@gmail.com>
2013-08-13 20:09:50 -0700

    Create camera wrapper object to handle top-down entity tracking
    This TopDownCamera keeps the Camera given to it exactly on top of the
    Entity being tracked at a certain height.

---
+++ src/slartibartfast/top_down_camera.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"components"
+	"core"
+)
+
+type TopDownCamera struct {
+	// The Entity being tracked by this camera
+	trackingEntity *core.Entity
+
+	// Distance above the Entity this camera stays
+	trackingHeight float32
+
+	// Link back to the actual camera object we move around
+	camera *core.Camera
+
+	currentlyTracking bool
+}
+
+func NewTopDownCamera(camera *core.Camera) *TopDownCamera {
+	return &TopDownCamera{
+		camera:            camera,
+		currentlyTracking: true,
+	}
+}
+
+// SetTrackingHeight sets the distance in world units of how far this camera
+// will stay above the Entity being tracked (in the Y axis)
+func (self *TopDownCamera) SetTrackingHeight(height float32) {
+	self.trackingHeight = height
+}
+
+// TrackEntity takes the Entity this camera is supposed to keep it's eye on
+func (self *TopDownCamera) TrackEntity(entity *core.Entity) {
+	self.trackingEntity = entity
+}
+
+// PauseTracking turns off any position tracking of the current Entity
+func (self *TopDownCamera) PauseTracking() {
+	self.currentlyTracking = false
+}
+
+// ResumeTracking moves the camera back to the Entity being tracked and resumes
+// camera position updates
+func (self *TopDownCamera) ResumeTracking() {
+	self.currentlyTracking = true
+}
+
+// UpdatePosition calculates where this camera needs to be to stay tracking
+// on the Entity, handling any potential changes of the Entity's position
+func (self *TopDownCamera) UpdatePosition() {
+	if self.currentlyTracking {
+		entityPosition := components.GetTransform(self.trackingEntity).Position
+		entityPosition.Y += self.trackingHeight
+
+		self.camera.SetPosition(entityPosition)
+	}
+}
---
+++ src/slartibartfast/top_down_camera_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"core"
+	"github.com/stretchrcom/testify/assert"
+	"math3d"
+	"testing"
+)
+
+func Test_NewTopDownCamera(t *testing.T) {
+	camera := core.NewCamera()
+	tdCam := NewTopDownCamera(camera)
+
+	assert.NotNil(t, tdCam)
+	assert.Equal(t, camera, tdCam.camera)
+}
+
+func Test_TopDownCamera_TracksEntities(t *testing.T) {
+	camera := NewTopDownCamera(core.NewCamera())
+	entity := core.NewEntity()
+	camera.TrackEntity(entity)
+
+	assert.Equal(t, entity, camera.trackingEntity)
+}
+
+func Test_TopDownCamera_UpdatesPositionToMatchEntity(t *testing.T) {
+	baseCam := core.NewCamera()
+	camera := NewTopDownCamera(baseCam)
+	camera.TrackEntity(core.NewEntityAt(math3d.Vector{10, 10, 10}))
+
+	camera.UpdatePosition()
+
+	assert.Equal(t, math3d.Vector{10, 10, 10}, baseCam.Position())
+}
+
+func Test_TopDownCamera_UpdatePositionCanKeepAHeightFromTheEntity(t *testing.T) {
+	baseCam := core.NewCamera()
+	camera := NewTopDownCamera(baseCam)
+	camera.TrackEntity(core.NewEntityAt(math3d.Vector{10, 10, 10}))
+	camera.SetTrackingHeight(10)
+
+	camera.UpdatePosition()
+
+	assert.Equal(t, math3d.Vector{10, 20, 10}, baseCam.Position())
+}
+
+func Test_TopDownCamera_CanPauseAndResumeTracking(t *testing.T) {
+	baseCam := core.NewCamera()
+	camera := NewTopDownCamera(baseCam)
+	camera.TrackEntity(core.NewEntityAt(math3d.Vector{10, 10, 10}))
+	camera.PauseTracking()
+
+	camera.UpdatePosition()
+	assert.Equal(t, math3d.Vector{0, 0, 0}, baseCam.Position())
+
+	camera.ResumeTracking()
+
+	camera.UpdatePosition()
+	assert.Equal(t, math3d.Vector{10, 10, 10}, baseCam.Position())
+}
